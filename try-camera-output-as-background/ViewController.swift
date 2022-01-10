2b083330b514a474e90169360a0eb8bf38aefa39
Rudi Farkas <rudi.farkas@gmail.com>
2022-01-10 21:06:58 +0100

    Initial Commit

---
+++ try-camera-output-as-background/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  try-camera-output-as-background
+//
+//  Created by Rudolf Farkas on 10.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
